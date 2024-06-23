9fa5021cf8178be6007b6f6e98a4737ee6db3f36
neginkheirmand <neginkheirmand@gmail.com>
2024-06-23 22:54:22 +0330

    add consumer

---
+++ consumer.py
@@ -0,0 +1,18 @@
+from kafka import KafkaConsumer
+import json
+
+# Create a Kafka consumer instance
+consumer = KafkaConsumer(
+    'my_topic',
+    bootstrap_servers=['localhost:29092', 'localhost:39092'],
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='my_group',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+)
+
+# Poll for new messages from Kafka and print them
+for message in consumer:
+    print(f"Received message: key={message.key} value={message.value}")
+
+consumer.close()
