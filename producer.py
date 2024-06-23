0b34f908482c23ffbd270ebb03ed1af571f1a128
neginkheirmand <neginkheirmand@gmail.com>
2024-06-23 22:54:33 +0330

    add producer

---
+++ producer.py
@@ -0,0 +1,16 @@
+from kafka import KafkaProducer
+import json
+
+# Create a Kafka producer instance
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:29092', 'localhost:39092'],
+    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+)
+
+# Produce some messages
+for i in range(10):
+    message = {'key': str(i), 'value': 'my_message_{}'.format(i)}
+    producer.send('my_topic', value=message)
+    producer.flush()
+
+print("Messages sent!")
